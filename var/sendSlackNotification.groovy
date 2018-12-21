8814230bc9e3252d91d5957a63687c169f03e03e
Ho Jun Lee <zeallat94@gmail.com>
2018-12-21 16:09:49 +0900

    Create sendSlackNotification.groovy

---
+++ var/sendSlackNotification.groovy
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy
+
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
+
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = "Build for ${env.JOB_NAME} [${env.BUILD_NUMBER}] is ${buildStatus}! Check console output at (${env.BUILD_URL})"
+
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESS') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+
+  // Send notifications
+  slackSend (color: colorCode, message: details)
+  telegramSend "${details}"
+}
